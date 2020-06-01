eace3a3c5ba995e7ad40af8d9fff1e8e922b4d6c
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-06-01 00:28:22 -0300

    add saveState method and creating string constants

--- app/src/main/java/com/pluralsight/notekeeper/NoteActivity.java
+++ app/src/main/java/com/pluralsight/notekeeper/NoteActivity.java
@@ -3,6 +3,7 @@ package com.pluralsight.notekeeper;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
@@ -98,6 +99,14 @@ public class NoteActivity extends AppCompatActivity {
             saveNote();
         }
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (outState != null) {
+            mViewModel.saveState(outState);
+        }
     }
 
     //set each of the original values back
--- app/src/main/java/com/pluralsight/notekeeper/NoteActivityViewModel.java
+++ app/src/main/java/com/pluralsight/notekeeper/NoteActivityViewModel.java
@@ -1,11 +1,22 @@
 package com.pluralsight.notekeeper;
 
+import android.os.Bundle;
+
 import androidx.lifecycle.ViewModel;
 
 public class NoteActivityViewModel extends ViewModel {
+    //we create string constant when we have to pass values into an intent
+    public static final String ORIGINAL_NOTE_COURSE_ID = "com.pluralsight.notekeeper.ORIGINAL_NOTE_COURSE_ID";
+    public static final String ORIGINAL_NOTE_TITLE= "com.pluralsight.notekeeper.ORIGINAL_NOTE_TITLE";
+    public static final String ORIGINAL_NOTE_TEXT = "com.pluralsight.notekeeper.ORIGINAL_NOTE_TEXT";
+
     //Make the fields public so we can use them from other classes
     public String mOriginalNoteCourseId;
     public String mOriginalNoteTitle;
     public String mOriginalNoteText;
 
+    public void saveState(Bundle outState) {
+        //Save the values into the bundle
+
+    }
 }
